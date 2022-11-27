0e1aff9889f058d4e2a0ef08a674b3a07d8ef073
romijatmiko <romijatmiko033@gmail.com>
2022-11-27 19:54:03 +0700

    ganti folder

--- JURA FULLSTACK/JURA CHICKEN/jura-chicken/src/components/menus/menu.js
+++ JURA FULLSTACK/JURA CHICKEN/jura-chicken/src/components/menus/menu.js
@@ -7,7 +7,7 @@ export const Menus = ({ menu }) => {
 	return (
 		<Col md={4} xs={6} className="mb-4">
 			<Card className="shadow">
-				<Card.Img variant="top" src={"img" + menu.img} />
+				<Card.Img variant="top" src={"./img/" + menu.img} />
 				<Card.Body>
 					<Card.Title>{menu.nama_menu}</Card.Title>
 					<Card.Text>Rp. {numberWithCommas(menu.harga_menu)}</Card.Text>
--- JURA FULLSTACK/JURA CHICKEN/jura-chicken/src/components/navbarj.js
+++ JURA FULLSTACK/JURA CHICKEN/jura-chicken/src/components/navbarj.js
@@ -1,39 +1,45 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from './logo.png';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "./logo.png";
 
 export function NavbarJura() {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Container>
-      <Navbar.Brand href="/">
-            <img
-              src={logo}
-              width="60"
-              height="60"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">All Menu</Nav.Link>
-            <Nav.Link href="/keranjang">Keranjang</Nav.Link>
-            <NavDropdown title="E-Jura" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Top Up E-Jura</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Cek Riwayat Top Up</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Laporkan Masalah</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/register">Daftar</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+	return (
+		<Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+			<Container>
+				<Navbar.Brand href="/">
+					<img
+						src={logo}
+						width="60"
+						height="60"
+						className="d-inline-block align-top"
+						alt="React Bootstrap logo"
+					/>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link href="/">All Menu</Nav.Link>
+						<Nav.Link href="/keranjang">Keranjang</Nav.Link>
+						<NavDropdown title="E-Jura" id="collasible-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">
+								Top Up E-Jura
+							</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">
+								Cek Riwayat Top Up
+							</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">
+								Laporkan Masalah
+							</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+					<Nav>
+						<Nav.Link href="/login">Login</Nav.Link>
+						<Nav.Link href="/register">Daftar</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
