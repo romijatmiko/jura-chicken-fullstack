d4847be8c89572397d2b4a56009ea3a9836ba149
romijatmiko <romijatmiko033@gmail.com>
2022-12-01 00:13:57 +0700

    yey bisa

--- JURA FULLSTACK/JURA CHICKEN SERVER/controllers/auth.js
+++ JURA FULLSTACK/JURA CHICKEN SERVER/controllers/auth.js
@@ -1,5 +1,5 @@
 import User from "../models/UserModel.js";
-import argon2 from "argon2";
+import bcrypt from "bcryptjs";
 
 export const Login = async (req, res) => {
 	const user = await User.findOne({
@@ -8,14 +8,19 @@ export const Login = async (req, res) => {
 		},
 	});
 	if (!user) return res.status(404).json({ msg: "User tidak ditemukan" });
-	const match = await argon2.verify(user.password, req.body.password);
+	const match = await bcrypt.compare(user.password, req.body.password);
 	if (!match) return res.status(400).json({ msg: "Wrong Password" });
 	req.session.userId = user.uuid;
 	const uuid = user.uuid;
-	const name = user.name;
-	const email = user.email;
+	const nama_user = user.nama_user;
+	const email_user = user.email_user;
+	const created_at = user.created_at;
+	const updated_at = user.updated_at;
 	const role = user.role;
-	res.status(200).json({ uuid, name, email, role });
+
+	res
+		.status(200)
+		.json({ uuid, nama_user, email_user, created_at, updated_at, role });
 };
 
 export const Me = async (req, res) => {
@@ -23,7 +28,14 @@ export const Me = async (req, res) => {
 		return res.status(401).json({ msg: "Mohon login ke akun Anda!" });
 	}
 	const user = await User.findOne({
-		attributes: ["uuid", "name", "email", "role"],
+		attributes: [
+			"uuid",
+			"nama_user",
+			"email_user",
+			"created_at",
+			"updated_at",
+			"role",
+		],
 		where: {
 			uuid: req.session.userId,
 		},
